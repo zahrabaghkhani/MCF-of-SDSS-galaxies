329fda61f06b838d0d90412b77a627138615d41a
Zahra Baghkhani <47903409+zahrabaghkhani@users.noreply.github.com>
2022-09-11 10:20:16 +0430

    Add files via upload

---
+++ random generator_SDSS.py
@@ -0,0 +1,19 @@
+#This code computes random catalogue for galaxies in the SDSS data!
+#For creating ra,dec it used randomSDSS package! For creating redshift it uses n(z) of original data!
+
+
+#ra, dec
+import randomsdss
+from scipy.stats import gaussian_kde
+ra, dec = randomsdss.sky_random(dr="DR16", catalog="SDSS", size=500000)
+
+def randz(z, randcatsize):
+    #Method to calculate random redshift values from input redshift distribution
+
+    pdf = gaussian_kde(z)
+    z_rand = pdf.resample(size= randcatsize)
+    z_rand = randzv.reshape(-1,1)
+    z_rand = randzv.reshape(-1)
+
+    return z_rand
+
